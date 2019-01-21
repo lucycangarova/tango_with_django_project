5878ff8e989fee1085ecc306a8605671aa36de99
Lucia Cangarova <2330954c@student.gla.ac.uk>
2019-01-21 21:56:43 +0000

    forth commit - after chapter 5

--- rango/views.py
+++ rango/views.py
@@ -11,5 +11,5 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	context_dict = {'boldmessage' : "This tutorial has been put together by Lucia Cangarova."}
+	context_dict = {'boldmessage' : "Lucia Cangarova"}
 	return render(request, 'rango/about.html', context=context_dict)
