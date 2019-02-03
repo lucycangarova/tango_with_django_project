436e258567b2789a5250fe28ea0ed94295a74f2d
Lucia Cangarova <2330954c@student.gla.ac.uk>
2019-02-03 20:29:10 +0000

    eighth commit - after chapter 9

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required 
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored. 
@@ -86,3 +87,68 @@ def add_page(request, category_name_slug):
 
 	context_dict = {'form':form, 'category' : category}
 	return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST) 
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save() 
+			user.set_password(user.password) 
+			user.save()
+
+			profile = profile_form.save(commit=False) 
+			profile.user = user
+
+			if 'picture' in request.FILES: 
+				profile.picture = request.FILES['picture'] 
+
+			profile.save()
+
+			registered = True
+		else:
+			# Invalid form
+			print(user_form.errors, profile_form.errors) 
+
+	else:
+		# These forms will be blank, ready for user input. 
+		user_form = UserForm() 
+		profile_form = UserProfileForm()
+
+	return render(request, 'rango/register.html', 
+		{'user_form': user_form, 'profile_form': profile_form, 'registered': registered}) 
+
+def user_login(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username') 
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if user: 
+			if user.is_active:
+				login(request, user) 
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("Your Rango account is disabled.") 
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password)) 
+			return render(request, 'rango/login.html', {'message': 'Invalid login details supplied.'}) 
+
+	else:
+		return render(request, 'rango/login.html', {}) 
+
+@login_required 
+def restricted(request): 
+	return render(request, 'rango/restricted.html', {})
+
+@login_required 
+def user_logout(request): 
+	# Since we know the user is logged in, we can now just log them out. 
+	logout(request) 
+	# Take the user back to the homepage. 
+	return HttpResponseRedirect(reverse('index')) 
